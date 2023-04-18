6a1200b55834f764da30ba66955e2cb08e7fd7a8
chey00 <c.hey@sbs-herzogenaurach.de>
2023-04-18 09:27:38 +0200

    Musterlösung

--- IPv6.py
+++ IPv6.py
@@ -1,5 +1,5 @@
 from PyQt6.QtWidgets import QWidget, QGridLayout, QLabel, QLineEdit, QTextBrowser
-from PyQt6.QtCore import Qt
+from PyQt6.QtCore import Qt, pyqtSlot
 
 
 class IPv6(QWidget):
@@ -9,8 +9,10 @@ class IPv6(QWidget):
         self.address = QLineEdit(self)
 
         # Aufgabe 1
+        self.address.setInputMask("<Hhhh:Hhhh:Hhhh:Hhhh:Hhhh:Hhhh:Hhhh:Hhhh/90;_")
 
         # Aufgabe 2
+        self.address.textChanged.connect(self.process_text)
 
         self.subnetmask = QTextBrowser(self)
         self.hex = QTextBrowser(self)
@@ -28,4 +30,21 @@ class IPv6(QWidget):
         self.setLayout(layout)
 
     # Aufgabe 2
+    @pyqtSlot(str)
+    def process_text(self, text: str):
         # Aufgabe 3
+        split_str = text.split("/")
+
+        if split_str[1]:
+            self.subnetmask.setText(split_str[1])
+
+        if split_str[0]:
+            fields = split_str[0].split(":")
+
+            memory = "0x"
+            for field in fields:
+                memory += field
+
+            self.hex.setText(memory)
+
+            #self.hex.setText("0x" + split_str[0].replace(":", ""))
